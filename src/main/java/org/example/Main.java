77a53756ab2fbaada36b69492b7932af62df5e72
Usuario-PC <yahirroza594@outlook.com>
2023-05-28 15:12:02 -0600

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.example.controlador.ControladorEjemplo;
+import org.example.vista.Ventana;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        Ventana ventana = new Ventana("Ejemplo JOPtionPane");
+        ControladorEjemplo controller = new ControladorEjemplo(ventana);
+    }
+}
